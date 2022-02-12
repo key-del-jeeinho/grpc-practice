f2972e37870bb593ba5dec8fb14ba9136ceefca6
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-12 17:40:17 +0900

    Initial commit

---
+++ grpc-client/src/test/java/com/velocia/grpcclient/GrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velocia.grpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
