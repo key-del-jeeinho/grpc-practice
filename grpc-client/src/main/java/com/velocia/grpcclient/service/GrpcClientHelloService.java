6084bafe31dca424879949fd044e9b7c26ad11d5
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-12 20:37:47 +0900

    [ADD] GrpcClient 를 통한 HelloService 구현체 작성

---
+++ grpc-client/src/main/java/com/velocia/grpcclient/service/GrpcClientHelloService.java
@@ -0,0 +1,19 @@
+package com.velocia.grpcclient.service;
+
+import com.velocia.lib.GetHelloMessageRequest;
+import com.velocia.lib.GetHelloMessageResponse;
+import com.velocia.lib.HelloGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrpcClientHelloService implements HelloService{
+    @GrpcClient("GLOBAL")
+    private HelloGrpc.HelloBlockingStub helloStub;
+
+    public String hello(final String name) {
+        GetHelloMessageRequest request = GetHelloMessageRequest.newBuilder().setName(name).build();
+        GetHelloMessageResponse response = helloStub.getHelloMessage(request);
+        return response.getMessage();
+    }
+}
